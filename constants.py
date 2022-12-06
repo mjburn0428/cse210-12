5dbbf23aa22fed1a0e943ee957366df8a1d5cc4a
meetkome <kvictory1010@gmail.com>
2022-12-06 06:17:04 +0100

    added files

--- constants.py
+++ constants.py
@@ -92,14 +92,14 @@ SCORE_FORMAT = "SCORE: {}"
 
 # BALL
 BALL_GROUP = "balls"
-BALL_IMAGE = "batter/assets/images/000.png"
+BALL_IMAGE = "shooting/assets/images/000.png"
 BALL_WIDTH = 28
 BALL_HEIGHT = 28
 BALL_VELOCITY = 6
 
 # RACKET
 RACKET_GROUP = "rackets"
-RACKET_IMAGES = [f"batter/assets/images/{n:03}.png" for n in range(100, 103)]
+RACKET_IMAGES = [f"shooting/assets/images/{n:03}.png" for n in range(100, 103)]
 RACKET_WIDTH = 106
 RACKET_HEIGHT = 28
 RACKET_RATE = 6
@@ -108,10 +108,10 @@ RACKET_VELOCITY = 7
 # BRICK
 BRICK_GROUP = "bricks"
 BRICK_IMAGES = {
-    "b": [f"batter/assets/images/{i:03}.png" for i in range(10,19)],
-    "g": [f"batter/assets/images/{i:03}.png" for i in range(20,29)],
-    "p": [f"batter/assets/images/{i:03}.png" for i in range(30,39)],
-    "y": [f"batter/assets/images/{i:03}.png" for i in range(40,49)]
+    "b": [f"shooting/assets/images/{i:03}.png" for i in range(10,19)],
+    "g": [f"shooting/assets/images/{i:03}.png" for i in range(20,29)],
+    "p": [f"shooting/assets/images/{i:03}.png" for i in range(30,39)],
+    "y": [f"shooting/assets/images/{i:03}.png" for i in range(40,49)]
 }
 BRICK_WIDTH = 80
 BRICK_HEIGHT = 28
