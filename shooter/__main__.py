56d4f0a08db4e67bf5a4de19ac223d4f1c07c7e0
meetkome <kvictory1010@gmail.com>
2022-12-07 15:27:04 +0100

    new commit

--- shooter/__main__.py
+++ shooter/__main__.py
@@ -3,37 +3,39 @@ import os
 import time
 import random
 pygame.font.init()
+
+from constants import *
 from game.casting.actor import Actor
 from game.casting.player import Player
 # from game.services.keyboard_service import KeyboardService
 
-# SCREEN
-# WIDTH, HEIGHT = 750, 750
-SCREEN_WIDTH = 950
-SCREEN_HEIGHT = 750
-CENTER_X = SCREEN_WIDTH / 2
-CENTER_Y = SCREEN_HEIGHT / 2
+# # SCREEN
+# # WIDTH, HEIGHT = 750, 750
+# SCREEN_WIDTH = 950
+# SCREEN_HEIGHT = 750
+# CENTER_X = SCREEN_WIDTH / 2
+# CENTER_Y = SCREEN_HEIGHT / 2
 
 
-WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Shooter game")
+# WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# pygame.display.set_caption("Shooter game")
 
-# Load images
-RED_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_blue_small.png"))
+# # Load images
+# RED_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_red_small.png"))
+# GREEN_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_green_small.png"))
+# BLUE_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_blue_small.png"))
 
-# Player player
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_yellow.png"))
+# # Player player
+# YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("shooter/assets", "pixel_ship_yellow.png"))
 
-# Lasers
-RED_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_yellow.png"))
+# # Lasers
+# RED_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_red.png"))
+# GREEN_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_green.png"))
+# BLUE_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_blue.png"))
+# YELLOW_LASER = pygame.image.load(os.path.join("shooter/assets", "pixel_laser_yellow.png"))
 
-# Background
-BG = pygame.transform.scale(pygame.image.load(os.path.join("shooter/assets", "background-black.png")), (SCREEN_WIDTH, SCREEN_HEIGHT))
+# # Background
+# BG = pygame.transform.scale(pygame.image.load(os.path.join("shooter/assets", "background-black.png")), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 
